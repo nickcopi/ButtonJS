4dd89a9c8bdcc362d3b3c6016e9a9c67d9fd9a93
nick <nick@noemail4unonon.comlol>
2020-12-20 18:06:26 -0500

    Add a button manager that handles clicks and renders registered buttons.

--- Button.js
+++ Button.js
@@ -14,12 +14,26 @@ let ButtonManager;
 		}
 		onClick(e){
 			this.buttons.forEach(button=>{
-
+				if(this.collide(button, {
+					x: e.offsetX,
+					y: e.offsetY,
+					width:1,
+					height:1
+				}))
+					button.click();
 			});
 		}
 		addButton(button){
 			this.buttons.push(button);
+			return button;
 		}
+		collide(o1,o2){
+			return (o1.x < o2.x + o2.width && o1.x + o1.width > o2.x && o1.y < o2.y + o2.height && o1.y + o1.height > o2.y) && o1 !== o2;
+		}
+		render(ctx){
+			this.buttons.forEach(button=>button.render(ctx));
+		}
+
 	}
 
 	ButtonRenderer = class {
